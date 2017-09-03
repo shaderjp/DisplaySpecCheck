d8a9f624259367afb5093e32d1eaeb0b98b983e7
Masafumi Takahashi <shader@shader.jp>
2017-09-03 12:50:38 +0900

    1st Commit

---
+++ D3DGetDisplaySpec/D3DGetDisplaySpec.cpp
@@ -0,0 +1,13 @@
+﻿// D3DGetDisplaySpec.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+Microsoft::WRL::ComPtr<IDXGISwapChain4> g_swapChain;
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ D3DGetDisplaySpec/stdafx.h
@@ -0,0 +1,23 @@
+﻿// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <windows.h>
+
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include <d2d1_3.h>
+#include <dwrite.h>
+#include <d3d11on12.h>
+#include <DirectXMath.h>
+
+#include <wrl.h>
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください
